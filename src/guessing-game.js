6445eaf28de29e6aecb9c8bde2f36b06273ab3c2
Kate Shpakovskaya <kate.shpakovskaya02@gmail.com>
2022-07-11 08:40:48 +0300

    feat: binary search>?

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
+        this.mid = Math.ceil((max + min) / 2)
     }
 
     guess() {
-
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid-1
+        this.mid = Math.ceil((this.max + this.min) / 2)
     }
 
     greater() {
-
+        this.min = this.mid+1
+        this.mid = Math.ceil((this.max + this.min) / 2)
     }
 }
 
